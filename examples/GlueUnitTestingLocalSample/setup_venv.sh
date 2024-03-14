f4704e16e96a3e026f49f7cae0ab2812956016d9
Gonzalo Herreros <gherreros@users.noreply.github.com>
2024-03-14 15:25:51 +0000

    Added batch companion

--- examples/GlueUnitTestingLocalSample/setup_venv.sh
+++ examples/GlueUnitTestingLocalSample/setup_venv.sh
@@ -9,10 +9,18 @@ if ! command -v python3 > /dev/null; then
   exit 1
 fi
 
+if ! command -v git > /dev/null; then
+  echo "This script requires Git to be installed and included in the PATH"
+  exit 1
+fi
+
 python3 -m venv venv
 echo "Created venv"
 source venv/bin/activate
-pip install pyspark==3.3.0
+echo "Installing Python modules"
+pip install pyspark==3.3.0 pytest pytest-cov
+pip install git+https://github.com/awslabs/aws-glue-libs.git
+
 PYSPARK_PATH=$(python -c '
 import pyspark
 import os
@@ -23,9 +31,6 @@ echo "Replacing jars with the ones from Glue 4"
 JARS_PATH="$PYSPARK_PATH/jars"
 mv $JARS_PATH ${JARS_PATH}_bak | true
 mvn -f configuration/pom.xml dependency:copy-dependencies -DoutputDirectory=${JARS_PATH}
-echo "Installing Glue module from Github"
-pip install git+https://github.com/awslabs/aws-glue-libs.git
-pip install pytest pytest-cov
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "Done, to run the test you can run 'pytest' once you have activated the venv using 'source venv/bin/activate'"
 echo "---------------------------------------------------------------------------------------------------------------------"
